0c0d73f36365290d4bf2fc8d68591bea4e0949b7
Suyeba <134159687+Suyeba@users.noreply.github.com>
2023-09-18 15:49:28 +0530

    Update test.py

--- test.py
+++ test.py
@@ -48,6 +48,10 @@ def predict_major_incident(day_of_week, location, attack_type, perpetrator, vict
                            country):
     # Create a dictionary to map categorical values to their one-hot encoded representations
     category_mapping = {
+        'day_of_week': ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'],
+        'location': ['mumbai', 'athens', 'paris', 'new york', 'sydney', 'seoul', 'tokyo', 'berlin', 'mexico city', 'bangkok', 'moscow',
+                     'beijing', 'toronto', 'lima', 'rio de janeiro', 'cape town', 'madrid', 'istanbul', 'dubai', 'nairobi', 'rome',
+                     'london', 'cairo', 'buenos aires', 'jakarta'],
         'attack_type': ['shooting', 'bombing', 'kidnapping', 'hijacking', 'stabbing', 'arson', 'assassination'],
         'perpetrator': ['Group A', 'Group B', 'Group C', 'Group D', 'Group E', 'Group F', 'Group G',
                        'Group H', 'Group I', 'Group J', 'Group K', 'Group L', 'Group M', 'Group N',
@@ -62,32 +66,62 @@ def predict_major_incident(day_of_week, location, attack_type, perpetrator, vict
         'motive': ['Religious', 'Ethnic', 'Retaliation Motive', 'Political', 'Unknown'],
         'operational_success': ['Yes', 'No'],
         'financial_support': ['International', 'Local', 'Unknown']
+        'country': ['turkey', 'kenya', 'peru', 'japan', 'france', 'usa', 'brazil', 'spain',
+                    'south africa', 'thailand', 'mexico', 'egypt', 'germany', 'australia', 'india',
+                    'uk', 'indonesia', 'argentina', 'south korea', 'russia', 'canada', 'italy',
+                    'uae', 'greece', 'china']
     }
 
     # Initialize the input_data with zeros for all features
     input_data = np.zeros(205)
 
     # Set the corresponding one-hot encoded feature values to 1
-    input_data[category_mapping['attack_type'].index(attack_type)] = 1
-    input_data[len(category_mapping['attack_type']) + category_mapping['perpetrator'].index(perpetrator)] = 1
-    input_data[len(category_mapping['attack_type']) + len(category_mapping['perpetrator']) + category_mapping[
-        'target_type'].index(target_type)] = 1
-    input_data[len(category_mapping['attack_type']) + len(category_mapping['perpetrator']) + len(
-        category_mapping['target_type']) + category_mapping['weapon_used'].index(weapon_used)] = 1
-    input_data[len(category_mapping['attack_type']) + len(category_mapping['perpetrator']) + len(
+    input_data[category_mapping['day_of_week'].index(day_of_week)] = 1
+
+    input_data[len(category_mapping['day_of_week']) + category_mapping['location'].index(location)] = 1
+
+    input_data[len(category_mapping['day_of_week']) + len(category_mapping['location']) + category_mapping[
+        'attack_type'].index(attack_type)] = 1
+
+    input_data[len(category_mapping['day_of_week']) + len(category_mapping['location']) + len(
+        category_mapping['attack_type']) + category_mapping['perpetrator'].index(perpetrator)] = 1
+
+    input_data[len(category_mapping['day_of_week']) + len(category_mapping['location']) + len(
+        category_mapping['attack_type']) + len(category_mapping['perpetrator']) + category_mapping['target_type'].index(
+        target_type)] = 1
+
+    input_data[len(category_mapping['day_of_week']) + len(category_mapping['location']) + len(
+        category_mapping['attack_type']) + len(category_mapping['perpetrator']) + len(category_mapping['target_type']) +
+        category_mapping['weapon_used'].index(weapon_used)] = 1
+
+    input_data[len(category_mapping['day_of_week']) + len(category_mapping['location']) + len(
+        category_mapping['attack_type']) + len(category_mapping['perpetrator']) + len(
         category_mapping['target_type']) + len(category_mapping['weapon_used']) + category_mapping['claimed_by'].index(
         claimed_by)] = 1
-    input_data[len(category_mapping['attack_type']) + len(category_mapping['perpetrator']) + len(
+
+    input_data[len(category_mapping['day_of_week']) + len(category_mapping['location']) + len(
+        category_mapping['attack_type']) + len(category_mapping['perpetrator']) + len(
         category_mapping['target_type']) + len(category_mapping['weapon_used']) + len(category_mapping['claimed_by']) +
-               category_mapping['motive'].index(motive)] = 1
-    input_data[len(category_mapping['attack_type']) + len(category_mapping['perpetrator']) + len(
+        category_mapping['motive'].index(motive)] = 1
+
+    input_data[len(category_mapping['day_of_week']) + len(category_mapping['location']) + len(
+        category_mapping['attack_type']) + len(category_mapping['perpetrator']) + len(
         category_mapping['target_type']) + len(category_mapping['weapon_used']) + len(
         category_mapping['claimed_by']) + len(category_mapping['motive']) + category_mapping[
-                   'operational_success'].index(operational_success)] = 1
-    input_data[len(category_mapping['attack_type']) + len(category_mapping['perpetrator']) + len(
+        'operational_success'].index(operational_success)] = 1
+
+    input_data[len(category_mapping['day_of_week']) + len(category_mapping['location']) + len(
+        category_mapping['attack_type']) + len(category_mapping['perpetrator']) + len(
         category_mapping['target_type']) + len(category_mapping['weapon_used']) + len(
         category_mapping['claimed_by']) + len(category_mapping['motive']) + len(
         category_mapping['operational_success']) + category_mapping['financial_support'].index(financial_support)] = 1
+
+    input_data[len(category_mapping['day_of_week']) + len(category_mapping['location']) + len(
+        category_mapping['attack_type']) + len(category_mapping['perpetrator']) + len(
+        category_mapping['target_type']) + len(category_mapping['weapon_used']) + len(
+        category_mapping['claimed_by']) + len(category_mapping['motive']) + len(
+        category_mapping['operational_success']) + len(category_mapping['financial_support']) +
+        category_mapping['country'].index(country)] = 1
 
     # Add other numerical features to the input_data
     input_data[196] = victims_injured
